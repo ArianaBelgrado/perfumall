931e63a3ad2c5a0b76a8ba88f5673933be3a8d97
manuelurruti <manuelurrutimarquez@gmail.com>
2022-11-15 14:26:57 -0300

    mergiexd

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,157 +1,154 @@
-const path = require('path');
-const bcryptjs = require('bcryptjs');
-const fs = require('fs');
+const path = require("path");
+const bcryptjs = require("bcryptjs");
+const fs = require("fs");
 
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const User = require('../models/User.js');
+const User = require("../models/User.js");
 
-const usersFilePath = path.join(__dirname, '../database/user.json');
-let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, "../database/user.json");
+let users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 const controlador = {
-    login: (req, res) => {
-        res.render('login');
-        // res.redirect('/');
-    },
+  login: (req, res) => {
+    res.render("login");
+    // res.redirect('/');
+  },
 
-    loginProcess: (req, res) => {
-        console.log(req.body);
-        let userToLogin = User.findByField('mail', req.body.mail);
-        console.log(userToLogin);
-        if (userToLogin) {
-            let passwordOk = bcryptjs.compareSync(req.body.password, userToLogin.password);
-            if (passwordOk) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-                return res.redirect('/');
-            }
-            return res.render('login', {
-                errors: {
-                    email: {
-                        msg: 'Las credenciales son inválidas',
-                    },
-                },
-            });
-        }
+  loginProcess: (req, res) => {
+    console.log(req.body);
+    let userToLogin = User.findByField("mail", req.body.mail);
+    console.log(userToLogin);
+    if (userToLogin) {
+      let passwordOk = bcryptjs.compareSync(
+        req.body.password,
+        userToLogin.password
+      );
+      if (passwordOk) {
+        delete userToLogin.password;
+        req.session.userLogged = userToLogin;
+        return res.redirect("/");
+      }
+      return res.render("login", {
+        errors: {
+          email: {
+            msg: "Las credenciales son inválidas",
+          },
+        },
+      });
+    }
 
-        return res.render('login', {
-            errors: {
-                email: {
-                    msg: 'No se encuentra registrado este email',
-                },
-            },
-        });
-    },
+    return res.render("login", {
+      errors: {
+        email: {
+          msg: "No se encuentra registrado este email",
+        },
+      },
+    });
+  },
 
-    profile: (req, res) => {
-        return res.render('profile', { user: req.session.userLogged });
-    },
-    logout: (req, res) => {
-        req.session.destroy();
-        return res.redirect('/');
-    },
-
-<<<<<<< HEAD
   profile: (req, res) => {
     return res.render("profile", { user: req.session.userLogged });
   },
-=======
-    editar: (req, res) => {
-        res.render('editar-perfil');
-        res.redirect('/');
-    },
-    // Create - Form to create
-    create: (req, res) => {
-        res.render('crear');
-    },
-    // Create -  Method to store
-    store: (req, res) => {
-        const resultValidation = validationResult(req);
->>>>>>> 978313dedac03633c9b9750edb7362fa9a4dff9f
+  logout: (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+  },
 
-        if (resultValidation.errors.length > 0) {
-            res.render('crear', {
-                errors: resultValidation.mapped(),
-                oldData: req.body,
-            });
-        }
-        let userInDB = User.findByField('mail', req.body.mail);
-        if (userInDB) {
-            return res.render('crear', {
-                errors: { mail: { msg: 'Este mail ya fue registrado' } },
-                oldData: req.body,
-            });
-        }
+  editar: (req, res) => {
+    res.render("editar-perfil");
+    res.redirect("/");
+  },
+  // Create - Form to create
+  create: (req, res) => {
+    res.render("crear");
+  },
+  // Create -  Method to store
+  store: (req, res) => {
+    const resultValidation = validationResult(req);
 
-        let nuevoUsuario = {
-            id: users[users.length - 1].id + 1,
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            ciudad: req.body.ciudad,
-            provincia: req.body.provincia,
-            password: bcryptjs.hashSync(req.body.contra, 10),
-            imagenPerfil: req.file.filename,
-            mail: req.body.mail,
-        };
-        users.push(nuevoUsuario);
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-        return res.redirect('/usuario/login');
-    },
-    // Update - Form to edit
-    change: (req, res) => {
-        let idUsuario = req.params.id;
-        let objetoUsuario;
-        for (let obj of users) {
-            if (idUsuario == obj.id) {
-                objetoUsuario = obj;
-                break;
-            }
-        }
-        res.render('editar-perfil.ejs', { usuario: objetoUsuario });
-        res.redirect('/');
-    },
-    // Update - Method to update
-    update: (req, res) => {
-        let idUsuario = req.params.id;
-        let objetoUsuario;
-        for (let obj of users) {
-            if (idUsuario == obj.id) {
-                obj.nombre = req.body.nombre;
-                obj.apellido = req.body.apellido;
-                obj.usuario = req.body.usuario;
-                obj.ciudad = req.body.ciudad;
-                obj.provincia = req.body.provincia;
-                obj.contraseña = req.body.contraseña;
-                obj.mail = req.body.mail;
-                break;
-            }
-        }
-        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
-        res.redirect('/');
-    },
+    if (resultValidation.errors.length > 0) {
+      res.render("crear", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+    }
+    let userInDB = User.findByField("mail", req.body.mail);
+    if (userInDB) {
+      return res.render("crear", {
+        errors: { mail: { msg: "Este mail ya fue registrado" } },
+        oldData: req.body,
+      });
+    }
 
-    borrar: (req, res) => {
-        let id = req.params.id;
+    let nuevoUsuario = {
+      id: users[users.length - 1].id + 1,
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      ciudad: req.body.ciudad,
+      provincia: req.body.provincia,
+      password: bcryptjs.hashSync(req.body.contra, 10),
+      imagenPerfil: req.file.filename,
+      mail: req.body.mail,
+    };
+    users.push(nuevoUsuario);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+    return res.redirect("/usuario/login");
+  },
+  // Update - Form to edit
+  change: (req, res) => {
+    let idUsuario = req.params.id;
+    let objetoUsuario;
+    for (let obj of users) {
+      if (idUsuario == obj.id) {
+        objetoUsuario = obj;
+        break;
+      }
+    }
+    res.render("editar-perfil.ejs", { usuario: objetoUsuario });
+    res.redirect("/");
+  },
+  // Update - Method to update
+  update: (req, res) => {
+    let idUsuario = req.params.id;
+    let objetoUsuario;
+    for (let obj of users) {
+      if (idUsuario == obj.id) {
+        obj.nombre = req.body.nombre;
+        obj.apellido = req.body.apellido;
+        obj.usuario = req.body.usuario;
+        obj.ciudad = req.body.ciudad;
+        obj.provincia = req.body.provincia;
+        obj.contraseña = req.body.contraseña;
+        obj.mail = req.body.mail;
+        break;
+      }
+    }
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+    res.redirect("/");
+  },
 
-        usuario = users.filter((p) => {
-            return p.id != id;
-        });
+  borrar: (req, res) => {
+    let id = req.params.id;
 
-        fs.writeFileSync(usersFilePath, JSON.stringify(usuario, null, ' '));
-        res.redirect('/');
-    },
-    // // Delete - Delete one product from DB
-    // destroy: (req,res) => {
-    //     let idUsuario = req.params.id;
+    usuario = users.filter((p) => {
+      return p.id != id;
+    });
 
-    // 	let arregloUsuarios = users.filter(function(elemento){
-    // 		return elemento.id!=idUsuario;
-    // 	})
+    fs.writeFileSync(usersFilePath, JSON.stringify(usuario, null, " "));
+    res.redirect("/");
+  },
+  // // Delete - Delete one product from DB
+  // destroy: (req,res) => {
+  //     let idUsuario = req.params.id;
 
-    // 	fs.writeFileSync(usersFilePath, JSON.stringify(arregloUsuarios, null, " "));
+  // 	let arregloUsuarios = users.filter(function(elemento){
+  // 		return elemento.id!=idUsuario;
+  // 	})
 
-    // 	res.redirect('/');
-    // }
+  // 	fs.writeFileSync(usersFilePath, JSON.stringify(arregloUsuarios, null, " "));
+
+  // 	res.redirect('/');
+  // }
 };
 module.exports = controlador;
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,47 +1,48 @@
-let express = require('express');
-const guestMiddleware = require('../middlewares/guestMiddleware');
-const usuarioController = require('../controllers/usuarioController');
-const validations = require('../middlewares/userRegisterValidations');
-const uploadFile = require('../middlewares/userMulter');
+let express = require("express");
+const guestMiddleware = require("../middlewares/guestMiddleware");
+const usuarioController = require("../controllers/usuarioController");
+const validations = require("../middlewares/userRegisterValidations");
+const uploadFile = require("../middlewares/userMulter");
 
 let router = express.Router();
 
 /*** CREATE PERFIL ***/
-router.get('/create', guestMiddleware, usuarioController.create);
-router.post('/create', uploadFile.single('imagenPerfil'), validations, usuarioController.store);
+router.get("/create", guestMiddleware, usuarioController.create);
+router.post(
+  "/create",
+  uploadFile.single("imagenPerfil"),
+  validations,
+  usuarioController.store
+);
 
 // // /*** EDIT ONE USUARIO ***/
-router.get('/change/:id', usuarioController.change);
-router.put('/change/:id', usuarioController.change);
+router.get("/change/:id", usuarioController.change);
+router.put("/change/:id", usuarioController.change);
 
 // // /*** DELETE ONE USUARIO***/
 // router.delete('/delete/:id', usuarioController.destroy);
 
 // // /*** EDIT ONE USUARIO  ***/
-router.get('/change/:id', usuarioController.change);
-router.put('/change/:id', usuarioController.change);
+router.get("/change/:id", usuarioController.change);
+router.put("/change/:id", usuarioController.change);
 
 // LOG IN
 
-router.get('/login', usuarioController.login);
+router.get("/login", usuarioController.login);
 
 // PROCESAR EL LOGIN
-router.post('/login', usuarioController.loginProcess);
+router.post("/login", usuarioController.loginProcess);
 
 // Perfil
 
-<<<<<<< HEAD
 router.get("/profile", usuarioController.profile);
-=======
-router.get('/profile', usuarioController.profile);
-router.get('/logout', usuarioController.logout);
->>>>>>> 978313dedac03633c9b9750edb7362fa9a4dff9f
+router.get("/logout", usuarioController.logout);
 
 // EDITAR PERFIL
 
-router.get('/editar', usuarioController.editar);
+router.get("/editar", usuarioController.editar);
 
 //BORRAR PERFIL
-router.delete('/borrar/:id', usuarioController.borrar);
+router.delete("/borrar/:id", usuarioController.borrar);
 
 module.exports = router;

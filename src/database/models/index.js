217a6505e57d2b086f91a4b2dd49360f2a5118ba
manuelurruti <manuelurrutimarquez@gmail.com>
2022-11-22 01:42:47 -0300

    asd

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -33,6 +33,7 @@ Object.keys(db).forEach(modelName => {
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
 
 module.exports = db;

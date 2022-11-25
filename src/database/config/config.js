80e238ca86ac08ffd47c7fc5a6ea610b400b2e8a
manuelurruti <manuelurrutimarquez@gmail.com>
2022-11-25 01:22:20 -0300

    hehco

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,12 @@
 module.exports={
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "perfumalldb",
-    "host": "127.0.0.1",
+    "username":process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql",
-    "port": 3306
+    "operatorsAliases":0
   },
   "test": {
     "username": "root",

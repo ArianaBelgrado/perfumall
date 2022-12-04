7589232bf2650024d75d8ef88a2f27f2d32b01ed
Juanzor <hjuan7217@gmail.com>
2022-12-04 17:15:35 -0300

    MARCA FUNCIONA JAJAJA

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,9 +2,9 @@ const db = require("../database/models");
 
 let controller = {
     home: (req, res) => {
-        db.Producto.findAll(/* { include: [{ association: 'marca' }] }*/)
+        db.Producto.findAll({ include: "marca" })
             .then((result) => {
-                // res.json(result);
+                console.log(result);
                 return res.render("home", { productos: result });
             })
             .catch((err) => console.log(err));
--- src/database/models/Marca.js
+++ src/database/models/Marca.js
@@ -4,6 +4,7 @@ module.exports = function (sequelize, DataTypes) {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
+            autoIncrement: true
         },
         nombre: {
             type: DataTypes.STRING(100),

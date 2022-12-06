c1f72ae91b53d61051fc7e4dc567801c7e9915db
Juanzor <hjuan7217@gmail.com>
2022-12-05 23:43:07 -0300

    Deploy3

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,11 +16,12 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   },
 };
 

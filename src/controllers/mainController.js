f8ac82a3c36a6825d172fef5f5d3ba39df8420f0
manuelurruti <manuelurrutimarquez@gmail.com>
2022-12-27 01:50:23 -0300

    ahora hay filtrado de productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -9,6 +9,71 @@ let controller = {
             })
             .catch((err) => console.log(err));
     },
+
+    filtradoAvon: async (req, res) => {
+
+        try {
+            const productos = await db.Producto.findAll({
+                include: "marca",
+                where: { marca_id: 1 }
+            })
+            return res.render("home", {
+                productos
+            })
+
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    },
+    filtradoNatura: async (req, res) => {
+
+        try {
+            const productos = await db.Producto.findAll({
+                include: "marca",
+                where: { marca_id: 2 }
+            })
+            return res.render("home", {
+                productos
+            })
+
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    },
+    filtradoHerencia: async (req, res) => {
+
+        try {
+            const productos = await db.Producto.findAll({
+                include: "marca",
+                where: { marca_id: 3 }
+            })
+            return res.render("home", {
+                productos
+            })
+
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    },
+    filtradoCh: async (req, res) => {
+
+        try {
+            const productos = await db.Producto.findAll({
+                include: "marca",
+                where: { marca_id: 4 }
+            })
+            return res.render("home", {
+                productos
+            })
+
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    },
 };
 
 module.exports = controller;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,6 +4,10 @@ let router = express.Router();
 
 router.get("/", mainController.home);
 
+router.get("/avon", mainController.filtradoAvon);
+router.get("/natura", mainController.filtradoNatura);
+router.get("/ch", mainController.filtradoCh);
+router.get("/herencia", mainController.filtradoHerencia);
 module.exports = router;
 
 

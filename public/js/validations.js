4362b67775b03e6773af5da5050c675aac6cb470
Juanzor <hjuan7217@gmail.com>
2023-01-25 05:27:49 -0300

    dd

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,77 +1,67 @@
-window.addEventListener("load", function () {
-  let formulario = document.querySelector("form.validations")
+let formulario = document.querySelector("form.validations");
 
-  formulario.addEventListener("submit", function (e) {
-    let errores = [];
-    let campoNombre = document.querySelector("input#nombre")
+let errores = [];
+let arraySinRepetidos;
+const validar = (campo) => {
+    errores.push(campo);
+    console.log(campo);
+};
 
-    if (campoNombre.value == "") {
-      errores.push("Tenes que compeltar el nombre")
-    }
-    else if (campoNombre.value.length <= 3) {
-      errores.push("El nombre debe contener 3 o mas letras")
-    }
+formulario.addEventListener("submit", function (e) {
+    let campoNombre = document.querySelector("input#nombre").value;
 
+    if (campoNombre == "" || campoNombre.length <= 3)
+        validar("El nombre debe contener 3 o mas letras");
 
-    let apellido = document.querySelector("input#apellido")
-    if (apellido.value == "") {
-      errores.push("Debes poner tu apellido")
-    }
-    else if (apellido.value.length <= 3) {
-      errores.push("Tu apellido debe tener al menos 3 carácteres")
+    let apellido = document.querySelector("input#apellido").value;
+    if (apellido == "" || apellido.length <= 3)
+        validar("El apellido debe contener 3 o mas letras");
 
-    }
+    let email = document.querySelector("input#email").value;
+    if (email == "") validar("Debes completar el mail");
 
-    let email = document.querySelector("input#email")
-    if (email.value == "") {
-      errores.push("Debes poner tu email")
-    }
+    let password = document.getElementById("password").value;
+    if (password == "" || password.length <= 5)
+        validar("Tu contraseña debe tener al menos 6 carácteres");
 
-
-    let password = document.getElementById("password")
-    if (password.value == "") {
-      errores.push("Debes aclarar una contraseña")
-    }
-    else if (password.value.length <= 5) {
-      errores.push("Tu contraseña debe tener al menos 6 carácteres")
-    }
-
-    let image = document.getElementById("image")
-    if (image.value == "") {
-      errores.push("Debes subir una imagen")
-    }
+    let image = document.getElementById("image").value;
+    if (image == "") validar("Debes subir una imagen");
 
     function validateFile() {
-      let allowedExtension = ['jpeg', 'jpg'];
-      let fileExtension = document.getElementById('image').value.split('.').pop().toLowerCase();
-      let isValidFile = false;
+        let allowedExtension = ["jpeg", "jpg"];
+        let fileExtension = document
+            .getElementById("image")
+            .value.split(".")
+            .pop()
+            .toLowerCase();
+        let isValidFile = false;
 
-      for (var index in allowedExtension) {
-
-        if (fileExtension === allowedExtension[index]) {
-          isValidFile = true;
-          break;
+        for (var index in allowedExtension) {
+            if (fileExtension === allowedExtension[index]) {
+                isValidFile = true;
+                break;
+            }
         }
-      }
 
-      return isValidFile;
+        return isValidFile;
     }
-    if (!validateFile()) {
-      errores.push("Esto tiene que ser un archivo jpg o jepg")
-    }
+    if (!validateFile()) validar("Esto tiene que ser un archivo jpg o jepg");
+    erroresToSet = new Set(errores);
+    console.log(errores);
     if (errores.length > 0) {
-      console.log(errores)
-      e.preventDefault();
-      let ulErrores = document.querySelector("div.ulErrores ul");
-      console.log(ulErrores)
-      errores.forEach(e => {
-        ulErrores.innerHTML += '<li class= "alert alert-danger mb-2 lix">' + e + '</li>'
-      });
+        let erroresA = [...erroresToSet];
+        console.log(errores);
+        e.preventDefault();
+        let ulErrores = document.querySelector("div.ulErrores ul");
+        let arrayLi = [];
+        let error;
+        erroresA.forEach((e) => {
+            arrayLi.push(
+                `<li class= "alert alert-danger mb-2 lix">' +${e}+ "</li>`
+            );
+        });
+        arrayLi.forEach((e) => {
+            ulErrores.append(e);
+        });
     }
-
-
-    errores = []
-  })
-})
-
-
+});
